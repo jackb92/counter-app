a15dfae9d341fcbcd53139a944adfa1e2bee5657
Jack Bentley <jack.bentley@ao.com>
2020-08-23 22:38:52 +0100

    Created functions for +/- buttons with tests

--- counter.js
+++ counter.js
@@ -1,22 +1,51 @@
 function incrementOne() {
     var value = document.getElementById("count")
-    var string = value.innerHTML
-    var number = parseInt(string)
+    var number = parseInt(value.innerHTML)
 
     if(number >= 10){
-        value.innerHTML = number.toString()
+        value.innerHTML = number
     }
-
     else {
         number++
-        value.innerHTML = number.toString()
+        value.innerHTML = number
     }
 }
 
 function decrementOne() {
     var value = document.getElementById("count")
-    var string = value.innerHTML
-    var number = parseInt(string)
-    number--
-    value.innerHTML = number.toString()
+    var number = parseInt(value.innerHTML)
+
+    if(number <= 0){
+        value.innerHTML = number
+    }
+    else {
+        number--
+        value.innerHTML = number
+    }
+}
+
+function incrementThree() {
+    var value = document.getElementById("count")
+    var number = parseInt(value.innerHTML)
+
+    if(number >= 7){
+        value.innerHTML = "10"
+    }
+    else {
+        number + 3
+        value.innerHTML = number + 3
+    }  
+}
+
+function decrementThree() {
+    var value = document.getElementById("count")
+    var number = parseInt(value.innerHTML)
+
+    if(number <= 3){
+        value.innerHTML = "0"
+    }
+    else {
+        number + 3
+        value.innerHTML = number - 3
+    }  
 }
--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -30,6 +30,7 @@ describe('counter app tests', () => {
     })
 
     it('should decrement number displayed by 1 when the - button is pressed', () => {
+        cy.countSetter(1)
         cy.get('#count').then(($count) => {
             const stringCount = $count.text()
             const numberCount = parseInt(stringCount)
@@ -45,27 +46,71 @@ describe('counter app tests', () => {
         })
     })
 
-    it('should not exceed 10 when + button is pressed', () => {
-        throw new Error("add code")
+    it('should not exceed 10 by default when + button is pressed', () => {
+        cy.countSetter(10)
+        cy.get(".plusOne").click()
+        cy.get("#count").contains("10")
     })
 
     it('should not go below 0 when - button is pressed', () => {
-        throw new Error("add code")
+        cy.countSetter(0)
+        cy.get(".minusOne").click()
+        cy.get("#count").contains("0")
     })
 
     it('should display button with +3', () => {
-        throw new Error("add code")
+        cy.get('.plusThree').contains('+3')
     })
 
     it('should display button with -3', () => {
-        throw new Error("add code")
+        cy.get('.minusThree').contains('-3')
     })
 
     it('should increment number displayed by 3 when the + button is pressed', () => {
-        throw new Error("add code")
+        cy.get('#count').then(($count) => {
+            const stringCount = $count.text()
+            const numberCount = parseInt(stringCount)
+            cy.get('.plusThree').click()
+            cy.get('#count').then(($newCount) => {
+                const newStringCount = $newCount.text()
+                const newNumberCount = parseInt(newStringCount)
+                if(newNumberCount === numberCount + 3){
+                    console.log('count incremented by 3')
+                }
+                else throw new Error ('count did not increment by 3')
+            })
+        })
     })
 
     it('should decrement number displayed by 3 when the - button is pressed', () => {
-        throw new Error("add code")
+        cy.get('#count').then(($count) => {
+            const stringCount = $count.text()
+            const numberCount = parseInt(stringCount)
+            cy.get('.minusThree').click()
+            cy.get('#count').then(($newCount) => {
+                const newStringCount = $newCount.text()
+                const newNumberCount = parseInt(newStringCount)
+                if(newNumberCount === numberCount - 3){
+                    console.log('count decremented by 3')
+                }
+                else throw new Error ('count did not decrement by 3')
+            })
+        })
+    })
+
+    it('should not exceed 10 by default when +3 button is pressed', () => {
+        cy.countSetter(9)
+        cy.get(".plusThree").click()
+        cy.get("#count").contains("10")
+    })
+
+    it('should not go below 0 when -3 button is pressed', () => {
+        cy.countSetter(2)
+        cy.get(".minusThree").click()
+        cy.get("#count").contains("0")
+    })
+
+    it('should not exceed customer upperbound when changed from default', () => {
+
     })
 })
